3c83ebf573d38506e0dbc75efd69641476727e02
FanTianyin <55476008+DerexFan@users.noreply.github.com>
2026-04-17 23:57:42 +1200

    Update echo messages in script.groovy

--- script.groovy
+++ script.groovy
@@ -1,13 +1,13 @@
 def buildApp(){
-  echo "building the application..."
+  echo "building the application from script..."
 }
 
 def TestApp(){
-  echo "testing the application..."
+  echo "testing the application from script..."
 }
 
 def deployApp(){
-  echo "deploying the application..."
+  echo "deploying the application from script..."
 }
 
 return this
